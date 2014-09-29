dcec0e2e97f34954cffa6c22af674ff34640577f
Christopher Bauer <Chrisbauer.career@gmail.com>
2014-09-28 23:04:16 -0400

    Update to thisdocument that displays comments for each date as a test example.

--- src/ContextualDates/ThisDocument.cs
+++ src/ContextualDates/ThisDocument.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
-using System.Windows.Forms.VisualStyles;
-using System.Xml.Linq;
 using Microsoft.Office.Tools.Word;
-using Microsoft.Office.Tools.Word.Controls;
-using Microsoft.VisualStudio.Tools.Applications.Runtime;
-
+using Label = System.Windows.Forms.Label;
 using Office = Microsoft.Office.Core;
 using Word = Microsoft.Office.Interop.Word;
 
@@ -20,18 +15,32 @@ namespace ContextualDates
     {
         private void ThisDocument_Startup(object sender, System.EventArgs e)
         {
-            
-            var activeWindowWidth = ActiveWindow.Width;
-            var width = activeWindowWidth/3;
-            var left = width;
-            var controlSite = Controls.AddControl(new Microsoft.Office.Tools.Word.Controls.Label
+            Content.Text = "This is 01/01/2014. Now it is 02/14/1987. Now it is 09/09/2014.";
+
+            BeforeSave += OnBeforeSave;
+        }
+
+        private void OnBeforeSave(object sender, SaveEventArgs saveEventArgs)
+        {
+            var text = Content.Text;
+            var myRegex = new Regex(@"\d{2}/\d{2}/\d{4}");
+            var totalLength = 0;
+            for (var i = 1; i <= Sentences.Count; i++)
             {
-                Text = @"Hey, welcome to Contextual Dates!",
-                Font = new Font("Arial", 40),
-                BorderStyle = BorderStyle.Fixed3D,
-                BackColor = Color.BlanchedAlmond
-            }, 0 , 50, width, 100,
-                "ContextualDateIdentifier");
+                var matchCollection = myRegex.Matches(Sentences[i].Text);
+                if (matchCollection.Count > 0)
+                {
+                    foreach (Match match in matchCollection)
+                    {
+                        object matchStartIndex = totalLength+match.Index;
+                        object matchEndIndex = ((int) matchStartIndex) + 10;
+                        Comments.Add(Range(ref matchStartIndex, ref matchEndIndex),
+                            string.Format("{0:d} is here", match.Value));
+                    }
+                    
+                }
+                totalLength += Sentences[i].Text.Count()+1; // Not sure why the +1 needs to be here, but there it is...
+            }
         }
 
 
